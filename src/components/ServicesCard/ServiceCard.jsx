df0c673cacedd21dee1d7be8502afb5b80c62e52
JPCali <161878420+JPCali@users.noreply.github.com>
2024-07-07 22:03:58 +0200

    Feat: se crean rutas y servicios

--- src/components/ServicesCard/ServiceCard.jsx
+++ src/components/ServicesCard/ServiceCard.jsx
@@ -1,8 +1,11 @@
 export function ServiceCard({ Services }) {
+  const onClickNameHandler = () => {
+    console.log(Services);
+  };
   return (
     <div>
       <img src=""></img>
-      <h2>Name :{Services.name}</h2>
+      <h2 onClick={onClickNameHandler}>Name :{Services.name}</h2>
       <p>Description: {Services.description}</p>
     </div>
   );
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,18 +3,18 @@ import { ServiceCard } from "../../components/ServicesCard/ServiceCard";
 
 export function Services() {
   const Services = [
-    { id: 1, name: "Service 1", description: "Description 1" },
-    { id: 2, name: "Service 2", description: "Description 2" },
-    { id: 3, name: "Service 3", description: "Description 3" },
-    { id: 4, name: "Service 4", description: "Description 4" },
-    { id: 5, name: "Service 5", description: "Description 5" },
+    { name: "Service 1", description: "Description 1" },
+    { name: "Service 2", description: "Description 2" },
+    { name: "Service 3", description: "Description 3" },
+    { name: "Service 4", description: "Description 4" },
+    { name: "Service 5", description: "Description 5" },
   ];
 
   return (
     <>
       <p>SERVICES</p>
-      {Services.map((element, index) => {
-        return <ServiceCard Services={element} />;
+      {Services.map((element) => {
+        return <ServiceCard key={element.name} Services={element} />;
       })}
     </>
   );
